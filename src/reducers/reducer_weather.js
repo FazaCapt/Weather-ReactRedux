218396d8cbb647e9f8bcfe80d21b275028ea8384
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-06-14 20:36:37 +0700

    Avoiding State Mutations in Reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,12 @@
-export default function(state = null, action) {
-    console.log('action recived', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    // console.log('action recived', action);
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // return state.push([action.payload.data]); 
+            return [ action.payload.data, ...state]; // [city, city, city] Not [city, [city, city]]
+    }
 
     return state;
 }
