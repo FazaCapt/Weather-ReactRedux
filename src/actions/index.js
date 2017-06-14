b1611e0e26f5db9d9d8ff6e8d50d6188f9864ce1
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-06-14 14:57:34 +0700

    Redux-Promise in Practice, mengambil data weather app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY ='526f3bdf694a57da1de2bbefe4692432';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
